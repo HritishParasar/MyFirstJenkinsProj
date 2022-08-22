c3fa75d4c68d661a641bfa8e388b036e21bb3f93
Hritish Parasar <you@example.com>
2022-08-22 20:21:59 +0530

    Jenkins file added

---
+++ JenkinsTrial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello....this is My First Jenkins Project");
+        }
+    }
+}
